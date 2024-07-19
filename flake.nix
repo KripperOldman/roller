99010454fb6720c0f4e979b44d9c76525a538d99
KripperOldman <binarywarrior76@gmail.com>
2024-07-20 04:38:17 +0500

    feat: added nix build

--- flake.nix
+++ flake.nix
@@ -5,9 +5,11 @@
     flake-utils.url = "github:numtide/flake-utils";
     zig-overlay.url = "github:mitchellh/zig-overlay";
     zls-overlay.url = "github:zigtools/zls/0.13.0";
+    gitignore.url = "github:hercules-ci/gitignore.nix";
+    gitignore.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, flake-utils, zig-overlay, zls-overlay, ... }:
+  outputs = { nixpkgs, flake-utils, zig-overlay, zls-overlay, gitignore, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -15,10 +17,36 @@
         zls = zls-overlay.packages.${system}.zls.overrideAttrs (old: {
           nativeBuildInputs = [ zig ];
         });
+        gitignoreSource = gitignore.lib.gitignoreSource;
       in
-      {
+      rec {
         devShells.default = pkgs.mkShell {
           packages = [ zig zls ];
+        };
+        packages.default = packages.roller;
+        packages.roller = pkgs.stdenvNoCC.mkDerivation {
+          name = "roller";
+          version = "master";
+          src = gitignoreSource ./.;
+          nativeBuildInputs = [ zig ];
+          dontConfigure = true;
+          dontInstall = true;
+          doCheck = true;
+          buildPhase = ''
+            mkdir -p .cache
+            zig build install \
+            --cache-dir $(pwd)/.zig-cache \
+            --global-cache-dir $(pwd)/.cache \
+            -Dcpu=baseline \
+            -Doptimize=ReleaseSafe \
+            --prefix $out
+          '';
+          checkPhase = ''
+            zig build test \
+            --cache-dir $(pwd)/.zig-cache \
+            --global-cache-dir $(pwd)/.cache \
+            -Dcpu=baseline
+          '';
         };
       });
 }
