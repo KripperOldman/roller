4fddf30fe78e4ac574c3a71ce17b54467531ac68
KripperOldman <binarywarrior76@gmail.com>
2024-08-17 11:01:09 +0500

    build: added `all` build step

--- build.zig
+++ build.zig
@@ -1,9 +1,18 @@
 const std = @import("std");
 
+const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+};
+
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -83,4 +92,39 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    const build_all_step = b.step("all", "Build for all targets");
+    for (targets) |t| {
+        const target_pcre2_dep = b.dependency("pcre2", .{
+            .target = b.resolveTargetQuery(t),
+            .optimize = optimize,
+        });
+        const target_pcre2_lib = target_pcre2_dep.artifact("pcre2-8");
+
+        const target_clap_dep = b.dependency("clap", .{
+            .target = b.resolveTargetQuery(t),
+            .optimize = optimize,
+        });
+        const target_clap_module = target_clap_dep.module("clap");
+
+        const target_exe = b.addExecutable(.{
+            .name = "roller",
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.resolveTargetQuery(t),
+            .optimize = .ReleaseSafe,
+        });
+
+        target_exe.root_module.linkLibrary(target_pcre2_lib);
+        target_exe.root_module.addImport("clap", target_clap_module);
+
+        const target_output = b.addInstallArtifact(target_exe, .{
+            .dest_dir = .{
+                .override = .{
+                    .custom = try t.zigTriple(b.allocator),
+                },
+            },
+        });
+
+        build_all_step.dependOn(&target_output.step);
+    }
 }
